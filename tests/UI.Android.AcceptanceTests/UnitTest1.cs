bdfe7f1873063a2bd98b30af0a4dc056384b4d55
Salvatore Campisano <1342203+sal-Camp@users.noreply.github.com>
2022-08-16 10:26:57 -0400

    Add clean project architecture

---
+++ tests/UI.Android.AcceptanceTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace UI.Android.AcceptanceTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
